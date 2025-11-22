39768bd2a8a55e8b984237e3f08974b358090bd0
ahmedali109 <ahmedalinaguib33@gmail.com>
2025-11-22 20:51:51 +0200

    chore: Update flutter_secure_storage to version 6.0.0 and change initial route to splash screen

--- lib/core/router/go_router.dart
+++ lib/core/router/go_router.dart
@@ -53,7 +53,7 @@ class AppNavigation {
 
   // GoRouter configuration
   static final GoRouter router = GoRouter(
-    initialLocation: AppPath.home,
+    initialLocation: AppPath.splash,
     debugLogDiagnostics: true, // Enable for debugging deep links
     navigatorKey: _rootNavigatorKey,
     redirect: (context, state) {
